ea223bf1715ab4b0e6963a085c2baf21aafa26c6
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2024-01-18 19:04:58 +0200

    add user list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import UserList from "./UserList";
 
+
 class App extends React.Component {
   render() {
-    return <UserList />;
+    return (
+      
+        <UserList />
+              
+    );
   }
 }
 
